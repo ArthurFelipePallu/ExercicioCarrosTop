9a921f9d4b7cd026dadf477f7a4594c2aeef0a51
Arthur Felipe Pallu <arthurfelipepallu@gmail.com>
2024-05-23 00:42:24 -0300

    Página e Componentes

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css"
+
+export default function Header()
+{
+    return(
+        <>
+            <div className="carros-top-header">
+                <p>Carros Top</p>
+            </div>
+        </>
+    );
+}
---
+++ src/routes/Catalogo/index.tsx
@@ -0,0 +1,10 @@
+import Header from "../../Components/Header";
+import "./styles.css"
+
+export default function Catalogo()
+{
+    return(
+        <Header />
+
+    );
+}
